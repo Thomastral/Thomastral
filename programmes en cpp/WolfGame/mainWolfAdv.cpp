5546948543351cfacc05c21933cc6d61953ebcbe
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-01-18 09:40:24 +0100

    Update mainWolfAdv.cpp

--- programmes en cpp/WolfGame/mainWolfAdv.cpp
+++ programmes en cpp/WolfGame/mainWolfAdv.cpp
@@ -1,3 +1,8 @@
+/*
+Program by : Thomas Paillard
+Wolf Game
+main
+*/
 #include "WolfAdv.h"
 #include <iostream>
 using namespace std;
@@ -12,7 +17,8 @@ int main()
         cout <<"1. Walk"<<endl;
         cout <<"2. Eat"<<endl;
         cout <<"3. Check Status"<<endl;
-        cout <<"4. Exit"<<endl;
+        cout <<"4. Choose wolf's name"<<endl;
+        cout <<"5. Exit"<<endl;
 
         int choice;
         cin >> choice;
@@ -32,8 +38,12 @@ int main()
         }
         else if (choice == 4) 
         {
-            break;
+            wolf.ChooseWN();
         }
+        else if (choice == 5)
+        {
+			break;
+		}
     }
     return 0;
 }
