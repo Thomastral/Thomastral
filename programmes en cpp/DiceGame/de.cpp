3e049e89a557a2222b48166a0931ac7ce696e656
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-01-27 13:27:45 +0100

    Create de.cpp

---
+++ programmes en cpp/DiceGame/de.cpp
@@ -0,0 +1,33 @@
+/*
+Program by : Thomas Paillard
+Dice Game
+fonctions
+*/
+#include "de.h"
+#include <iostream>
+#include <stdlib.h>		//srand, rand
+#include <time.h>
+using namespace std;
+
+//constructeur
+Dice::Dice() 
+{
+	A = 1;
+    B = 2;
+}
+
+void Dice::X()
+{
+	
+}
+
+void Dice::Y()
+{
+	
+}
+
+//destructeur
+Dice::~Dice()
+{
+	
+}
