69c745cd8e826e3fbb3f731123a74cb895027adc
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-01-27 13:27:22 +0100

    Create d.h

---
+++ programmes en cpp/DiceGame/d.h
@@ -0,0 +1,22 @@
+/*
+Program by : Thomas Paillard
+Dice Game
+* .h file
+*/
+#ifndef DICE_H
+#define DICE_H
+
+class Dice
+{
+  private:
+    int A;
+    int B;
+
+  public:
+	Dice();
+	void X();
+	void Y();
+    ~Dice();
+};
+
+#endif
