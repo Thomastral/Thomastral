94c7f13c6e7be3fbcaba37358cd027e6eacedff6
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-01-27 13:28:19 +0100

    Create mainde.cpp

---
+++ programmes en cpp/DiceGame/mainde.cpp
@@ -0,0 +1,21 @@
+/*
+Program by : Thomas Paillard
+Dice Game
+main
+*/
+#include "de.h"
+#include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+using namespace std;
+
+int main() 
+{
+    Dice dice;
+
+    while (true) 
+    {
+		
+    }
+    return 0;
+}
