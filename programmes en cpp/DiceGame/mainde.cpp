14cfefa06167b11fd78fcf465dfae89eb5dcbd8f
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-01-27 14:23:17 +0100

    Update mainde.cpp

--- programmes en cpp/DiceGame/mainde.cpp
+++ programmes en cpp/DiceGame/mainde.cpp
@@ -12,10 +12,11 @@ using namespace std;
 int main() 
 {
     Dice dice;
-
-    while (true) 
-    {
-		
-    }
+    int guess;
+    
+    cout << "Dice's face before the roll : " << dice.get_value() << endl;
+        
+    cout << "Dice's face after the roll : " << dice.roll() << endl;
+    
     return 0;
 }
