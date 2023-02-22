d0fd3c6a550e86cc4649b1766f2f008bcc6009ec
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-02-22 09:11:09 +0100

    Create jeu.cpp

---
+++ programmes en cpp/JeuDeDe/jeu.cpp
@@ -0,0 +1,26 @@
+/*
+Program by : Thomas Paillard
+Jeu de de
+fonctions
+*/
+#include "classejeu.h"
+#include <iostream>
+#include <stdlib.h>		//srand, rand
+#include <time.h>
+#include <string>
+using namespace std;
+
+//constructeur
+Jeu::Jeu() 
+{
+	
+}
+
+
+
+//destructeur
+Jeu::~Jeu()
+{
+	
+}
+
