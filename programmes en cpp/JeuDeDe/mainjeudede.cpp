04bcb31490551b84f9284889e58b620e73d9a4e6
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-03-01 08:56:59 +0100

    Update mainjeudede.cpp

--- programmes en cpp/JeuDeDe/mainjeudede.cpp
+++ programmes en cpp/JeuDeDe/mainjeudede.cpp
@@ -17,19 +17,24 @@ int main()
 	char pseudoA[1000];
 	char pseudoB[1000];
 	
-	printf("\n\n");
+	printf("\n\e[1;37m*********************************************************************************\e[0m\n\n");
+	
 	printf("\e[1;37mEnregistrement des joueurs\e[0m");
 	printf("\n\n\n");
 	
-    printf("\e[1;37mJoueur A, entrez votre pseudo : \e[0m");
+    printf("\e[37mJoueur A, entrez votre pseudo : \e[0m");
     scanf("%s", pseudoA);
     printf("\n\n");
     
-    printf("\e[1;37mJoueur B, entrez votre pseudo : \e[0m");
+    printf("\e[37mJoueur B, entrez votre pseudo : \e[0m");
     scanf("%s", pseudoB);
     printf("\n\n");
+    
+    printf("\e[1;37m********************************************************************************\e[0m\n\n");
 	
 	printf("\n");
+	printf("\e[1m********************************************************************************\e[0m\n\n");
+	
     cout << "\e[1mDebut de la partie\e[0m" << endl;
     printf("\n");
    
@@ -42,15 +47,27 @@ int main()
     
     //lance des De
     joueurA->jouer();
+	cout << "\e[37mTour de \e[0m" << joueurA->get_pseudo() << endl;
+	printf("\n");
+    cout << "\e[37mLe premier De est tombe sur la face \e[0m" << de1->get_valeur() << "\e[37m et le deuxieme De est tombe sur la face :\e[0m" << de2->get_valeur() << endl;
     cout << "" << joueurA->get_pseudo() << "\e[37m a obtenu un score de : \e[0m" << joueurA->get_point() << endl;
     printf("\n");
    
-    joueurB->jouer();    
+	printf("\e[37m********************************************************************************\e[0m\n\n");
+   
+    joueurB->jouer(); 
+	cout << "\e[37mTour de \e[0m" << joueurB->get_pseudo() << endl;
+	printf("\n");
+    cout << "\e[37mLe premier De est tombe sur la face \e[0m" << de1->get_valeur() << "\e[37m et le deuxieme De est tombe sur la face :\e[0m" << de2->get_valeur() << endl;  
     cout << "" << joueurB->get_pseudo() << "\e[37m a obtenu un score de : \e[0m" << joueurB->get_point() << endl;
-    printf("\n\n");
+    printf("\n");
+    
+    printf("\e[1m********************************************************************************\e[0m\n\n");
         
     delete de1;
     delete de2;
+    delete joueurA;
+    delete joueurB;
     
     return 0;
 }
