4bee5f6317d19ea8ccfc4cab5ea05f90614b9ce7
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-03-01 08:57:34 +0100

    Update jeu.cpp

--- programmes en cpp/JeuDeDe/jeu.cpp
+++ programmes en cpp/JeuDeDe/jeu.cpp
@@ -4,10 +4,13 @@ Jeu de de
 fonctions
 */
 #include "classejeu.h"
+#include "classejoueur.h"
+#include "classede.h"
 #include <iostream>
 #include <stdlib.h>		//srand, rand
 #include <time.h>
 #include <string>
+#include <list>
 using namespace std;
 
 //constructeur
@@ -16,11 +19,25 @@ Jeu::Jeu()
 	
 }
 
+int Jeu::getValDe1()
+{
+	return 0;
+}
 
+int Jeu::getValDe2()
+{
+	return 0;
+}
+
+int Jeu::get_tourActuel()
+{
+	return tourActuel;
+}
 
 //destructeur
 Jeu::~Jeu()
 {
+	
 	
 }
 
