ca36dcf04100142fe52c9d50082d41dc9831a845
Thomas <112658964+Thomastral@users.noreply.github.com>
2023-05-09 08:26:53 +0200

    Update Pong.py

--- Python/Pong/Pong.py
+++ Python/Pong/Pong.py
@@ -1,115 +1,69 @@
 import pygame
 import random
-
+ 
 # Initialisation de Pygame
 pygame.init()
-
-# Définition de la taille de la fenêtre
-screen_width = 640
-screen_height = 480
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-# Définition des couleurs
-black = (0, 0, 0)
-white = (255, 255, 255)
-
-# Définition des positions et des dimensions des éléments du jeu
-paddle_width = 15
-paddle_height = 60
-paddle_speed = 5
-
-ball_width = 15
-ball_speed = 5
-
-player1_x = 50
-player1_y = screen_height/2 - paddle_height/2
-
-player2_x = screen_width - 50 - paddle_width
-player2_y = screen_height/2 - paddle_height/2
-
-ball_x = screen_width/2 - ball_width/2
-ball_y = screen_height/2 - ball_width/2
-ball_direction_x = random.choice([-1, 1])
-ball_direction_y = random.choice([-1, 1])
-
-# Définition des scores des joueurs
-player1_score = 0
-player2_score = 0
-
+ 
+# Couleurs
+BLANC = (255, 255, 255)
+NOIR = (0, 0, 0)
+ 
+# Taille de l'écran
+LARGEUR = 800
+HAUTEUR = 600
+ 
+# Création de l'écran
+ecran = pygame.display.set_mode((LARGEUR, HAUTEUR))
+pygame.display.set_caption("Pong")
+ 
+# Création des raquettes et de la balle
+raquette1 = pygame.Rect(50, (HAUTEUR/2) - 40, 10, 80)
+raquette2 = pygame.Rect(LARGEUR - 60, (HAUTEUR/2) - 40, 10, 80)
+balle = pygame.Rect(LARGEUR/2 - 10, HAUTEUR/2 - 10, 20, 20)
+ 
+# Vitesse initiale de la balle
+vit_x = 5 * random.choice((1, -1))
+vit_y = 5 * random.choice((1, -1))
+ 
 # Boucle principale du jeu
-running = True
-while running:
-    # Gestion des événements
+jeu_en_cours = True
+while jeu_en_cours:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    # Gestion des mouvements des joueurs
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player1_y -= paddle_speed
-    if keys[pygame.K_s]:
-        player1_y += paddle_speed
-    if keys[pygame.K_UP]:
-        player2_y -= paddle_speed
-    if keys[pygame.K_DOWN]:
-        player2_y += paddle_speed
-
-    # Gestion des limites de l'écran pour les joueurs
-    if player1_y < 0:
-        player1_y = 0
-    if player1_y + paddle_height > screen_height:
-        player1_y = screen_height - paddle_height
-    if player2_y < 0:
-        player2_y = 0
-    if player2_y + paddle_height > screen_height:
-        player2_y = screen_height - paddle_height
-
-    # Gestion des mouvements de la balle
-    ball_x += ball_speed * ball_direction_x
-    ball_y += ball_speed * ball_direction_y
-
-    # Gestion des collisions avec les murs
-    if ball_y < 0 or ball_y + ball_width > screen_height:
-        ball_direction_y = -ball_direction_y
-    if ball_x < 0:
-        ball_direction_x = -ball_direction_x
-        player2_score += 1
-        ball_x = screen_width/2 - ball_width/2
-        ball_y = screen_height/2 - ball_width/2
-        ball_direction_x = random.choice([-1, 1])
-        ball_direction_y = random.choice([-1, 1])
-    if ball_x + ball_width > screen_width:
-        ball_direction_x = -ball_direction_x
-        player1_score += 1
-        ball_x = screen_width/2 - ball_width/2
-        ball_y = screen_height/2 - ball_width/2
-        ball_direction_x = random.choice([-1, 1])
-        ball_direction_y = random.choice([-1, 1
-    # Gestion des collisions avec les raquettes
-    if ball_x < player1_x + paddle_width and ball_y + ball_width > player1_y and ball_y < player1_y + paddle_height:
-        ball_direction_x = 1
-
-    if ball_x + ball_width > player2_x and ball_y + ball_width > player2_y and ball_y < player2_y + paddle_height:
-        ball_direction_x = -1
-
-    # Effacement de l'écran
-    screen.fill(black)
-
-    # Dessin des éléments du jeu
-    pygame.draw.rect(screen, white, (player1_x, player1_y, paddle_width, paddle_height))
-    pygame.draw.rect(screen, white, (player2_x, player2_y, paddle_width, paddle_height))
-    pygame.draw.rect(screen, white, (ball_x, ball_y, ball_width, ball_width))
-
-    # Affichage des scores des joueurs
-    font = pygame.font.Font(None, 36)
-    player1_score_text = font.render(str(player1_score), True, white)
-    player2_score_text = font.render(str(player2_score), True, white)
-    screen.blit(player1_score_text, (screen_width/4 - player1_score_text.get_width()/2, 20))
-    screen.blit(player2_score_text, (3*screen_width/4 - player2_score_text.get_width()/2, 20))
-
-    # Mise à jour de la fenêtre
+            jeu_en_cours = False
+ 
+    # Mouvement de la raquette 1
+    touche = pygame.key.get_pressed()
+    if touche[pygame.K_UP]:
+        raquette1.y -= 5
+    if touche[pygame.K_DOWN]:
+        raquette1.y += 5
+ 
+    # Mouvement de la raquette 2
+    if balle.y < raquette2.y:
+        raquette2.y -= 3
+    if balle.y > raquette2.y + 80:
+        raquette2.y += 3
+ 
+    # Mouvement de la balle
+    balle.x += vit_x
+    balle.y += vit_y
+ 
+    # Rebond de la balle
+    if balle.colliderect(raquette1) or balle.colliderect(raquette2):
+        vit_x = -vit_x
+    if balle.y < 0 or balle.y > HAUTEUR - 20:
+        vit_y = -vit_y
+ 
+    # Affichage de l'écran
+    ecran.fill(NOIR)
+    pygame.draw.rect(ecran, BLANC, raquette1)
+    pygame.draw.rect(ecran, BLANC, raquette2)
+    pygame.draw.ellipse(ecran, BLANC, balle)
     pygame.display.flip()
-
-# Fermeture de Pygame
+ 
+    # Vitesse du jeu
+    pygame.time.Clock().tick(60)
+ 
+# Quitter Pygame
 pygame.quit()
